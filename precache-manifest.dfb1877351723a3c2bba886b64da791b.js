b7a2585d1d4ac065292306e86fc9ce2a44c3f87b
Carlos Maldonado <kardaniel1998@gmail.com>
2020-08-28 08:24:32 -0500

    Updates

---
+++ precache-manifest.dfb1877351723a3c2bba886b64da791b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0607d19edaeb26c8e1fbc1b315e7031e",
+    "url": "/Proyecto-Iglesia/index.html"
+  },
+  {
+    "revision": "53206eb94a0f607e3950",
+    "url": "/Proyecto-Iglesia/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "bd189d87042bbe58954d",
+    "url": "/Proyecto-Iglesia/static/css/main.bd57b2e6.chunk.css"
+  },
+  {
+    "revision": "53206eb94a0f607e3950",
+    "url": "/Proyecto-Iglesia/static/js/2.b59b0ec0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Proyecto-Iglesia/static/js/2.b59b0ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd189d87042bbe58954d",
+    "url": "/Proyecto-Iglesia/static/js/main.195ca086.chunk.js"
+  },
+  {
+    "revision": "42395e045653fd71d822",
+    "url": "/Proyecto-Iglesia/static/js/runtime-main.046eb3fb.js"
+  },
+  {
+    "revision": "f77a613dc1f8b08deb7f536aa7d2877d",
+    "url": "/Proyecto-Iglesia/static/media/ImgDirec.f77a613d.jpg"
+  },
+  {
+    "revision": "b0ca9fc31b10c69aa2a5f97847954a45",
+    "url": "/Proyecto-Iglesia/static/media/ImgEquipo1.b0ca9fc3.jpg"
+  },
+  {
+    "revision": "1a004527811936569cdc68b80911d2a8",
+    "url": "/Proyecto-Iglesia/static/media/ImgSobreNosotros1.1a004527.jpg"
+  },
+  {
+    "revision": "8453c5e24df2c90ddffe0a3647681716",
+    "url": "/Proyecto-Iglesia/static/media/ImgSobreNosotros3.8453c5e2.jpg"
+  },
+  {
+    "revision": "e3a8e17b1f74390a03f7115e6169fb6f",
+    "url": "/Proyecto-Iglesia/static/media/avatar4.e3a8e17b.jpg"
+  },
+  {
+    "revision": "39b030929c7810e5aabc31eb4cacb091",
+    "url": "/Proyecto-Iglesia/static/media/avatar5.39b03092.jpg"
+  },
+  {
+    "revision": "133c3a97934bf44d8a5aaf525390e58e",
+    "url": "/Proyecto-Iglesia/static/media/avatar6.133c3a97.jpg"
+  }
+]);
